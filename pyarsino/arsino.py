48ddf8e42ca8204079e5959e4cbc29b2700c0b19
Xwybylty Soslan <soslanx@gmail.com>
2013-05-12 13:25:05 +0400

    arsino package added. draft

---
+++ pyarsino/arsino.py
@@ -0,0 +1,22 @@
+class ArsinoDevice:
+	
+	def __init__(self, device):
+		self.device=device
+		
+	def query(q):
+		return "making query"
+		
+	def digital_on(self, pin):
+		self.query("D"+str(pin)+"e"+"1")
+	
+	def digital_off(self, pin):
+		self.query("D"+str(pin)+"e"+"0")
+		
+	def get_digital(self, pin):
+		return self.query("d"+str(pin)+"e")
+	
+	def get_analog(self, pin):
+		return self.query("a"+str(pin)+"e")
+		
+	def set_analog(self, pin, value):
+		self.query("A"+str(pin)+"e"+str(value))
