c67ef002f1466a657650511b1070617ce829a756
Xwybylty Soslan <soslanx@gmail.com>
2013-05-12 21:19:18 +0400

    real query function, some other fixes

--- pyarsino/arsino.py
+++ pyarsino/arsino.py
@@ -1,10 +1,16 @@
+import serial
+
 class ArsinoDevice:
 	
 	def __init__(self, device):
-		self.device=device
+		self.device = device
+		self.serial = serial.Serial(device,9600)
 		
-	def query(q):
-		return "making query"
+	def query(self,q):
+		self.serial.flushInput()
+		self.serial.write(q)
+		response=self.serial.readline()
+		return response
 		
 	def digital_on(self, pin):
 		self.query("D"+str(pin)+"e1e")
@@ -13,10 +19,10 @@ class ArsinoDevice:
 		self.query("D"+str(pin)+"e0e")
 		
 	def get_digital(self, pin):
-		return self.query("d"+str(pin)+"e")
+		return int(self.query("d"+str(pin)+"e"))
 	
 	def get_analog(self, pin):
-		return self.query("a"+str(pin)+"e")
+		return float(self.query("a"+str(pin)+"e"))
 		
 	def set_analog(self, pin, value):
 		self.query("A"+str(pin)+"e"+str(value)+"e")
